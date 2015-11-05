875d1dce13f67de85d5258f289a104c15c745ac2
Chester Moy <cmoy@hawk.iit.edu>
2015-11-05 17:26:40 -0600

    new project

---
+++ app/src/test/java/com/cs442/team6/madbikes/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.cs442.team6.madbikes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
