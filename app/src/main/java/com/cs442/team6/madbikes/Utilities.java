1b12370af684fd12fe3ce200eb6ee17bef480eb6
Chester Moy <cmoy@hawk.iit.edu>
2015-11-13 17:47:11 -0600

    Utilities: Implement more functions
    Completed Functions:
    getPopularity()
    updatePopularity()
    hasVoted()
    authenticate()
    isAuthenticated()
    hashPasswd()
    
     Changes to be committed:
    	modified:   app/src/main/java/com/cs442/team6/madbikes/Utilities.java

--- app/src/main/java/com/cs442/team6/madbikes/Utilities.java
+++ app/src/main/java/com/cs442/team6/madbikes/Utilities.java
@@ -1,9 +1,14 @@
 package com.cs442.team6.madbikes;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by moymoy on 11/7/15.
@@ -13,6 +18,10 @@ public class Utilities {
 	Context c;
 	SQLiteDatabase db;
     SQLHelper dbhelper;
+
+    // Sharedpref AUTH flag
+    String AUTH_FILE = "auth";
+    String AUTH_FLAG = "isAuth";
 
 	Utilities(Context c, SQLHelper dbhelper) {
 		this.c = c;
@@ -50,10 +59,37 @@ public class Utilities {
 	}
 
     public void updatePopularity(int BID) {
+        String[] columns = {
+                dbhelper.VOTES.UID,
+                dbhelper.VOTES.BID
+        };
+        String selection = dbhelper.BIKES.BID + " = " + BID;
+        Cursor vcur = db.query(
+                dbhelper.BIKES.TABLE_NAME,
+                columns,
+                selection,
+                null,
+                null,
+                null,
+                null
+        );
+        if (vcur == null) {
+            vcur.close();
+            return;
+        }
 
+        columns = new String[]{
+                dbhelper.BIKES.BID,
+                dbhelper.BIKES.LIKES
+        };
+        ContentValues cvals = new ContentValues();
+        cvals.put(dbhelper.BIKES.LIKES, vcur.getCount());
+        db.update(dbhelper.BIKES.TABLE_NAME, cvals, selection, null);
+        vcur.close();
     }
 
     public boolean hasVoted(int UID, int BID) {
+        boolean ret;
         String[] columns = {
                 dbhelper.VOTES.UID
         };
@@ -68,12 +104,9 @@ public class Utilities {
                 null,
                 null
         );
-        if (cur.getCount() <= 0) {
-            cur.close();
-            return false;
-        }
+        ret = cur.getCount() <= 0 ? false:true;
         cur.close();
-        return true;
+        return ret;
     }
 
     public Location getLocation(int BID) {
@@ -81,10 +114,48 @@ public class Utilities {
     }
 
     public boolean authenticate(String username, String passwd) {
-        return false;
+        String hash = hashPasswd(passwd);
+        String[] columns = {
+                dbhelper.USERS.USERNAME,
+                dbhelper.USERS.PASSWORD
+        };
+        String selection = dbhelper.USERS.USERNAME + " = " + username
+                + "AND " + dbhelper.USERS.PASSWORD + " = " + hash;
+        Cursor cur = db.query(
+                dbhelper.BIKES.TABLE_NAME,
+                columns,
+                selection,
+                null,
+                null,
+                null,
+                null
+        );
+        if (cur.getCount() <= 0) {
+            cur.close();
+            return false;
+        }
+        cur.close();
+        SharedPreferences pref = c.getSharedPreferences(AUTH_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean(AUTH_FLAG, false);
+        editor.commit();
+        return true;
+    }
+
+    public String hashPasswd(String passwd) {
+        String ret = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(passwd.getBytes());
+            ret = new String(md.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return ret;
     }
 
     public boolean isAuthenticated() {
-        return false;
+        SharedPreferences pref = c.getSharedPreferences(AUTH_FILE, Context.MODE_PRIVATE);
+        return pref.getBoolean(AUTH_FLAG, false);
     }
 }
