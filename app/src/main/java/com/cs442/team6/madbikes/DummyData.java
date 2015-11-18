718d0fdbf3ddd680af4d96c7824e72dab119ae5d
Chester Moy <cmoy@hawk.iit.edu>
2015-11-18 13:44:06 -0600

    DummyData: Add more users and bikes to DummyData
     Changes to be committed:
    	modified:   DummyData.java

--- app/src/main/java/com/cs442/team6/madbikes/DummyData.java
+++ app/src/main/java/com/cs442/team6/madbikes/DummyData.java
@@ -20,6 +20,34 @@ public class DummyData {
         if (util.numUsers() > 0) {
             return;
         }
-        util.addUser("cshe", "Congwei She", "2255555", "123");
+        util.addUser("cshe", "Congwei She", "12255555", "123");
+        util.addUser("cmoy", "Chester Moy", "12255556", "234");
+        util.addUser("ggadhvi", "Gaurav Gadhvi", "12255557", "567");
+        util.addUser("aarshok", "Arjun Arshok", "12255558", "890");
+
+        util.addBike(
+                util.getUID("cmoy"),
+                "Black Allez Alluminum Body",
+                41.838310,
+                -87.627371,
+                "Brand new",
+                (float)5.00
+        );
+        util.addBike(
+                util.getUID("ggadhvi"),
+                "Fastest Bike GG no Re",
+                41.832620,
+                -87.615028,
+                "Brand new",
+                (float)5.00
+        );
+        util.addBike(
+                util.getUID("aarshok"),
+                "N/A",
+                41.837289,
+                -87.623354,
+                "USB doesn't work",
+                (float)5.00
+        );
     }
 }
