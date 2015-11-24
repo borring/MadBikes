5a036ca704d6490b2093eb830072f298670f2ed3
Congwei She <cshe@hawk.iit.edu>
2015-11-24 15:51:23 -0600

    Listofbikes: New activity

---
+++ app/src/main/java/com/cs442/team6/madbikes/ListOfBike.java
@@ -0,0 +1,21 @@
+package com.cs442.team6.madbikes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ListOfBike extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_of_bike);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+    }
+
+}
