730fb9665a9a61668549ae9f38f4889f888fc31f
forarjun <aashokk1@hawk.IIT.edu>
2015-12-11 14:00:04 -0600

    Dummy Data
    edited user data

--- app/src/main/java/com/cs442/team6/madbikes/DummyData.java
+++ app/src/main/java/com/cs442/team6/madbikes/DummyData.java
@@ -33,7 +33,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("cmoy@gmail.com"),
                 "Black Allez Alluminum Body",
-                "3001 s king drive Chicago",
+                "Stuart Building 10 West 31st Street Chicago",
                 41.838310,
                 -87.627371,
                 "New",
@@ -42,7 +42,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("cmoy@gmail.com"),
                 "Grey Allez Alluminum Body",
-                "3001 s king drive Chicago",
+                "Stuart Field Chicago",
                 41.838927, -87.628662,
                 "New",
                 (float)5.00
@@ -50,7 +50,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("cmoy@gmail.com"),
                 "White Allez Alluminum Body",
-                "3001 s king drive Chicago",
+                "3001 s wabash avenue Chicago",
                 41.839839, -87.625540,
                 "New",
                 (float)5.00
@@ -58,7 +58,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("ggadhvi@gmail.com"),
                 "Red Mangoose",
-                "2801 s king drive Chicago",
+                "3038 South Princeton Avenue Chicago",
                 41.838344, -87.634274,
                 "New",
                 (float)5.00
@@ -66,7 +66,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("ggadhvi@gmail.com"),
                 "Blue Mangoose",
-                "2801 s king drive Chicago",
+                "3102 S wells st Chicago",
                 41.838368, -87.633136,
                 "New",
                 (float)5.00
@@ -74,7 +74,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("aashokk@gmail.com"),
                 "Black Schwinn Wayfarer",
-                "2951 s king drive Chicago",
+                "2851 s king drive Chicago",
                 41.841592, -87.616292,
                 "Good",
                 (float)5.00
@@ -82,7 +82,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("aashokk@gmail.com"),
                 "Grey Schwinn Wayfarer",
-                "2951 s king drive Chicago",
+                "2901 s king drive Chicago",
                 41.840402, -87.616400,
                 "Good",
                 (float)5.00
@@ -90,7 +90,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("aashokk@gmail.com"),
                 "Blue Schwinn Wayfarer",
-                "2951 s king drive Chicago",
+                "IIT Krishna House S michigan avenue Chicago",
                 41.837287,
                 -87.623352,
                 "Good",
@@ -99,7 +99,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("cshe@gmail.com"),
                 "Pink Kulana Womens",
-                "2951 s federal street Chicago",
+                "2951 s king drive Chicago",
                 41.839366, -87.616206,
                 "Like New",
                 (float)15.00
@@ -107,7 +107,7 @@ public class DummyData {
         util.addBike(
                 util.getUID("cshe@gmail.com"),
                 "White Kulana Womens",
-                "2951 s federal street Chicago",
+                "dunbar vocational high school s king drive Chicago",
                 41.840593, -87.618068,
                 "Like New",
                 (float)15.00
