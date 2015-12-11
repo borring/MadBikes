100c0205b3453fa9107fe8d74aeb29a69c9b48df
forarjun <aashokk1@hawk.IIT.edu>
2015-12-10 22:38:16 -0600

    Dummy Data
    added more bikes and edited user data

--- app/src/main/java/com/cs442/team6/madbikes/DummyData.java
+++ app/src/main/java/com/cs442/team6/madbikes/DummyData.java
@@ -43,8 +43,7 @@ public class DummyData {
                 util.getUID("cmoy@gmail.com"),
                 "Grey Allez Alluminum Body",
                 "3001 s king drive Chicago",
-                41.838312,
-                -87.627373,
+                41.838927, -87.628662,
                 "New",
                 (float)5.00
         );
@@ -52,8 +51,7 @@ public class DummyData {
                 util.getUID("cmoy@gmail.com"),
                 "White Allez Alluminum Body",
                 "3001 s king drive Chicago",
-                41.838308,
-                -87.627369,
+                41.839839, -87.625540,
                 "New",
                 (float)5.00
         );
@@ -61,8 +59,7 @@ public class DummyData {
                 util.getUID("ggadhvi@gmail.com"),
                 "Red Mangoose",
                 "2801 s king drive Chicago",
-                41.832620,
-                -87.615028,
+                41.838344, -87.634274,
                 "New",
                 (float)5.00
         );
@@ -70,8 +67,7 @@ public class DummyData {
                 util.getUID("ggadhvi@gmail.com"),
                 "Blue Mangoose",
                 "2801 s king drive Chicago",
-                41.832622,
-                -87.615030,
+                41.838368, -87.633136,
                 "New",
                 (float)5.00
         );
@@ -79,8 +75,7 @@ public class DummyData {
                 util.getUID("aashokk@gmail.com"),
                 "Black Schwinn Wayfarer",
                 "2951 s king drive Chicago",
-                41.837289,
-                -87.623354,
+                41.841592, -87.616292,
                 "Good",
                 (float)5.00
         );
@@ -88,8 +83,7 @@ public class DummyData {
                 util.getUID("aashokk@gmail.com"),
                 "Grey Schwinn Wayfarer",
                 "2951 s king drive Chicago",
-                41.837291,
-                -87.623356,
+                41.840402, -87.616400,
                 "Good",
                 (float)5.00
         );
@@ -106,8 +100,7 @@ public class DummyData {
                 util.getUID("cshe@gmail.com"),
                 "Pink Kulana Womens",
                 "2951 s federal street Chicago",
-                42.837296,
-                -88.623360,
+                41.839366, -87.616206,
                 "Like New",
                 (float)15.00
         );
@@ -115,8 +108,7 @@ public class DummyData {
                 util.getUID("cshe@gmail.com"),
                 "White Kulana Womens",
                 "2951 s federal street Chicago",
-                42.837298,
-                -88.623362,
+                41.840593, -87.618068,
                 "Like New",
                 (float)15.00
         );
